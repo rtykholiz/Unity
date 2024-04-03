b3f385edc9fb06dd537ee9877feabb17cfa4ac93
Roma Tykholiz <mmy3uka@gmail.com>
2024-04-03 18:25:02 +0300

    Update EnemyMovement.cs

--- Enemy/EnemyMovement.cs
+++ Enemy/EnemyMovement.cs
@@ -7,9 +7,13 @@ public class EnemyMovement : MonoBehaviour
 
     public float testTimer;
     private float _testTimer;
+
+    private Tween _rotateTween;
+
+    private int _randomMethod = 0;
     private void Start()
     {
-        //Стартова швидкість
+        //start speed
         if (speed == 0)
         {
             transform.DORotate(new Vector3(0, 0, UnityEngine.Random.Range(0, 360)), 0);
@@ -18,40 +22,47 @@ public class EnemyMovement : MonoBehaviour
         _testTimer = testTimer;
 
         _check = false;
-        if (UnityEngine.Random.value > 0.5f)
+        if (Random.value > 0.5f)
         {
             _check = true;
-        } 
+        }
+
+        _randomMethod = Random.Range(1,3);
+        Debug.Log(gameObject.name + " - random is " + _randomMethod);
     }
-    // Update is called once per frame
     void Update()
     {
-        //Рухає персонажа (ворога), якщо true, то персонаж рухається за допомогою Perp
-        //MoveMethod1();
-        MoveMethod2();
+        switch (_randomMethod)
+        {
+            case 1:
+                MoveMethod1();
+                break;
+            case 2: MoveMethod2();
+                break;
+            default: Debug.Log("Method not selected");
+                break;
+        }
 
-       
+        //If touched, GO will destroy gameobject
+        if (Input.touchCount > 0)
+        {
+            OnTouchCheck();
+        }
     }
-    private void OnMouseDown()
+    private void OnTouchCheck()
     {
         Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
 
-        if (Input.touchCount > 0)
+        Touch _touch = Input.GetTouch(0);
+        if (_touch.phase == TouchPhase.Began)
         {
-            Touch _touch = Input.GetTouch(0);
-            if(_touch.phase == TouchPhase.Began)
+            if (hit.collider != null)
             {
-                Debug.Log("Touch");
-
-                if (hit.collider != null)
-                {
-                    Debug.Log("I'm hitting " + hit.collider.name);
-                }
+                Debug.Log("I'm hitting " + hit.collider.name);
+                Destroy(hit.collider.gameObject);
             }
         }
-        
-        
     }
 
     private void MoveMethod1()
@@ -64,9 +75,9 @@ public class EnemyMovement : MonoBehaviour
         }
         else
         {
-            testTimer = UnityEngine.Random.Range(3.5f, 7.5f);
-            var ttime = UnityEngine.Random.Range(0.75f, 2.5f);
-            transform.DORotate(new Vector3(0, 0, UnityEngine.Random.Range(0, 360)), ttime);
+            testTimer = Random.Range(3.5f, 7.5f);
+            var ttime = Random.Range(0.75f, 2.5f);
+            _rotateTween = transform.DORotate(new Vector3(0, 0, Random.Range(0, 360)), ttime);
         }
     }
 
@@ -90,13 +101,13 @@ public class EnemyMovement : MonoBehaviour
             if (_check)
             {
                 zEuler += _rotateAngle;
-                transform.DORotate(new Vector3(0, 0, zEuler), _rotateSpeed);
+                _rotateTween=  transform.DORotate(new Vector3(0, 0, zEuler), _rotateSpeed);
                 _check = false;
             }
             else
             {
                 zEuler -= _rotateAngle;
-                transform.DORotate(new Vector3(0, 0, zEuler), _rotateSpeed);
+                _rotateTween = transform.DORotate(new Vector3(0, 0, zEuler), _rotateSpeed);
                 _check = true;
             }
 
